3c8f1b5a465446f9db5acdf0a530368792c3224a
austin-darrow <adarrow@utexas.edu>
2022-07-27 16:36:56 -0500

    Fix major bugs; implement history

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+//CONSTANTS & VARIABLE ASSIGNMENT
+const display = document.querySelector('#display');
+const nums = document.querySelectorAll('.nums');
+const operators = document.querySelectorAll('.operators');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const history = document.querySelector('#history');
+
+let a;
+let b;
+let operation;
+let result;
+let enterPressed = false;
+display.textContent = '0';
+
 //BASIC FUNCTIONS
 function add(a,b) {
     return a+b;
@@ -18,49 +34,35 @@ function multiply(a,b) {
 function operate(operation,a,b) {
     a = Number(a);
     b = Number(b);
+    history.textContent = `${a} ${operation} ${b} =`;
     switch (operation) {
         case '+':
-            return add(a,b).toFixed(3);
+            return Number(add(a,b).toFixed(3));
         case '-':
-            return subtract(a,b).toFixed(3);
+            return Number(subtract(a,b).toFixed(3));
         case '/':
-            return divide(a,b).toFixed(3);
+            return Number(divide(a,b).toFixed(3));
         case '*':
-            return multiply(a,b).toFixed(3);
+            return Number(multiply(a,b).toFixed(3));
         default:
             return "OOPS";
     };
 };
 
-//CONSTANTS & VARIABLE ASSIGNMENT
-const display = document.querySelector('#display');
-const nums = document.querySelectorAll('.nums');
-const operators = document.querySelectorAll('.operators');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
-const backspace = document.querySelector('#backspace');
-const history = document.querySelector('#history');
-
-let a;
-let b;
-let operation;
-let result;
-
-
 //DISPLAY NUMBERS
 nums.forEach(num => {
     num.addEventListener('click', () => {
-        if (result) {
+        if (result) { //When pressing a number key, if there's already a result on display, set a to that, reset everything else
             a = result;
             b = undefined;
             result = undefined;
-            display.textContent = '';
+            display.textContent = '0';
         };
-        if (num.value === '.') {
-            if ((display.textContent + num.value)% 1 === 0)
-                display.textContent = display.textContent + num.value;
+        if (num.value === '.' && display.textContent.includes('.')) return
+        if (display.textContent === '0') {
+            display.textContent = num.value;
         } else {
-        display.textContent = display.textContent + num.value;
+            display.textContent = display.textContent + num.value;
         };
     });
 });
@@ -69,15 +71,22 @@ nums.forEach(num => {
 //MAIN OPERATION
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        if (a === undefined) {
+        if (a === undefined) { //if a hasn't been set, set a to the display, reset the display, set the operator to what was chosen
             a = display.textContent;
             display.textContent = '';
             operation = operator.value;
-        } else if (b === undefined) {
+            history.textContent = `${a} ${operation}`;
+        } else if (b === undefined) { //a has been set. If b hasn't, set b to display, operate, display the results, set the op to what was chosen
             b = display.textContent;
             result = operate(operation,a,b);
             display.textContent = result;
             operation = operator.value;
+            history.textContent = `${history.textContent.slice(0, -1)} ${operation}`;
+        } else { //a & b have been set, operate was performed with equals, result is set.
+            a = display.textContent;
+            display.textContent = '';
+            operation = operator.value;
+            history.textContent = `${history.textContent.slice(0, -1)} ${operation}`;
         };
     });
 });
@@ -89,70 +98,64 @@ equals.addEventListener('click', () => {
     b = display.textContent;
     result = operate(operation,a,b);
     display.textContent = result;
-})
+});
 
 backspace.addEventListener('click', () => {
-    let text = display.textContent.slice(0, -1);
-    display.textContent = text;
+    display.textContent = display.textContent.slice(0, -1);
 });
 
-clear.addEventListener('click', () => {
-    display.textContent = '';
-    resetAll();
-});
+clear.addEventListener('click', reset);
 
-function resetAll () {
-    a = undefined;
-    b = undefined;
-    operation = undefined;
-    result = undefined;
+function reset () {
+    location.reload()
 };
 
 
 //KEYBOARD FUNCTIONALITY
 function afterOp (e) {
-    if (result) {
-        a = undefined;
-        b = result;
-    };
     if (a === undefined) {
         a = display.textContent;
         display.textContent = '';
         operation = e.key;
-        if (b) {
-            result = operate(operation,a,b);
-            display.textContent = result;
-            a = undefined;
-            b = result;
-            result = undefined;
-        };
+        history.textContent = `${a} ${operation}`;
     } else if (b === undefined) {
         b = display.textContent;
         result = operate(operation,a,b);
         display.textContent = result;
         operation = e.key;
+        history.textContent = `${history.textContent.slice(0, -1)} ${operation}`;
+    } else {
+        a = display.textContent;
+        display.textContent = '';
+        operation = e.key;
+        history.textContent = `${history.textContent.slice(0, -1)} ${operation}`;
     };
 };
 
 window.addEventListener('keydown', (e) => {
-    if (result) {
-        a = result;
-        b = undefined;
-        result = undefined;
-        display.textContent = '';
+    if (isFinite(e.key) || e.key === '.') {
+        if (result) {
+            a = result;
+            b = undefined;
+            result = undefined;
+            display.textContent = '0';
+        };
+        if (e.key === '.' && display.textContent.includes('.')) return 
+        if (display.textContent === '0') {
+            display.textContent = e.key;
+        } else {display.textContent = display.textContent + e.key;
+        };
     };
-    if (e.key === '.') {
-        if (displayValue % 1 === 0)
-            display.textContent = display.textContent + e.key;
-    } else if (isFinite(e.key)) {
-        display.textContent = display.textContent + e.key;
-    } else if (e.key === 'Backspace') {
-        display.textContent = display.textContent.slice(0, -1);
-    } else if (e.key === 'Enter') {
+    if (e.key === '-' || e.key === '+' || e.key === '/' || e.key === '*') {
+        afterOp(e);
+    };
+    if (e.key === 'Enter' || e.key === '=') {
         b = display.textContent;
         result = operate(operation,a,b);
         display.textContent = result;
-    } else if (e.key === '-' || e.key === '+' || e.key === '/' || e.key === '*') {
-        afterOp(e);
     };
+    if (e.key === 'Backspace') {
+        display.textContent = display.textContent.slice(0, -1);
+    };
+    if (e.key === 'Escape') reset;
 });
